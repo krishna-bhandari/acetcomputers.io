48ff80643cf6095e07dda59cc065d100b46f8ae2
krishna-bhandari <59533492+krishna-bhandari@users.noreply.github.com>
2020-01-09 23:21:46 +0545

    new page added

--- desktop.php
+++ desktop.php
@@ -11,50 +11,49 @@
 </style>
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+$customer_nameErr = $addressErr = $contactErr = $deviceErr = $statusErr = "";
+$customer_name = $address = $contact = $status = $solution = $technician = $device = $device_detail = $entry_number = $date = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $entry_number=test_input($_POST["entry_number"]);
+    $date=test_input($_POST["date"]);
+    $device_detail=test_input($_POST["device_detail"]);
+    $solution=test_input($_POST["solution"]);
+    $technician=test_input($_POST["technician"]);
+
   if (empty($_POST["name"])) {
+   
     $nameErr = "Name is required";
   } else {
-    $name = test_input($_POST["name"]);
+    $name = test_input($_POST["customer_name"]);
     // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
+    if (!preg_match("/^[a-zA-Z ]*$/",$customer_name)) {
+      $customer_nameErr = "Only letters and white space allowed"; 
     }
   }
   
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
+  if (empty($_POST["address"])) {
+    $addressErr = "Address is required";
+  } else{
+    $address = test_input($_POST["address"]);
+
   }
     
-  // if (empty($_POST["website"])) {
-  //   $website = "";
-  // } else {
-  //   $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-  //   if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-  //     $websiteErr = "Invalid URL"; 
-  //   }
-  // }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
+  if (empty($_POST["contact"])) {
+    $contactErr = "Contact is required";
   } else {
-    $comment = test_input($_POST["comment"]);
+    $contact = test_input($_POST["contact"]);
+
+  if (empty($_POST["device"])) {
+    $deviceErr = "device is required";
+  } else {
+    $device = test_input($_POST["device"]);
   }
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
+  if (empty($_POST["status"])) {
+    $statusErr = "status is required";
   } else {
-    $gender = test_input($_POST["gender"]);
+    $status = test_input($_POST["status"]);
   }
 }
 
@@ -85,7 +84,9 @@ function test_input($data) {
         <li><a href="elements.php">Elements</a></li>
       </ul>
     </nav>
-
+      <div id="heading" >
+        <h1>Desktop entry</h1>
+      </div>
 <!-- <h2>PHP Form Validation Example</h2> -->
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -93,20 +94,18 @@ function test_input($data) {
 	<div class="row gtr-uniform">
     <div class="col-6 col-12-xsmall">
         <label for="entry number"><b>Entry Number</b></label>
-        <input type="text" placeholder="" name="customer_name" id="entry_number" value="" readonly=""><br>
-        <!-- <span class="error"> *<?php echo $nameErr;?></span> -->
+        <input type="text" placeholder="" name="entry_number" id="entry_number" value="" readonly=""><br>
     </div>
       <div class="col-6 col-12-xsmall">
         <label for="Date"><b>Date :</b></label>
         <input type="text" placeholder="" name="date" id="date" value="<?php
-echo "<b>".date("d/M/Y")."</b>" ;?>" readonly=""><br>
-        <!-- <span class="error"> *<?php echo $nameErr;?></span> -->
+              echo date("d/M/Y").date("D");?>" readonly=""><br>
     </div>
 		    	<!-- customer name input -->
 		<div class="col-6 col-12-xsmall">
 	      <label for="customer_name"><b>Customer Name</b></label>
 	      <input type="text" placeholder="Enter Customer Name" name="customer_name" id="name" value=""><br>
-	      <span class="error"> *<?php echo $nameErr;?></span>
+	      <span class="error"> *<?php echo $customer_nameErr;?></span>
 		</div>
 		<br><br>
 		      	<!-- contact input -->
@@ -125,32 +124,30 @@ echo "<b>".date("d/M/Y")."</b>" ;?>" readonly=""><br>
 		      	<!-- device selection -->
 		      	<div class="col-6 col-12-xsmall">
 		      <label for="psw"><b>Device :</b></label>
-		     	<select name="select" id="category">
+		     	<select name="device" id="category">
 						<option value="">- Select -</option>
-						<option value="">Motherboard</option>
-						<option value="">Full CPU</option>
-						<option value="">Monitor</option>
-						<option value="">Others</option>
+						<option value="Motherboard">Motherboard</option>
+						<option value="Full CPU">Full CPU</option>
+						<option value="Monitor">Monitor</option>
+						<option value="Others">Others</option>
 					</select>
 					<span class="error"> <?php echo $deviceErr;?></span>
 		<br><br>	
 		<!-- device detail -->
 		<div class="col-12 col-12-xsmall">
 		      <label for="device detail"><b>Device detail</b></label>
-		      <input type="text" placeholder="Enter device detail" name="device_detail" id="device_detail" value=""><br>
-		      <!-- <span class="error"> *<?php echo $Err;?></span></div> -->
+		      <input type="text" placeholder="Enter device detail" name="device_detail" id="device_detail" value=""><br></div>
 		<br><br>
-		      	<!-- password input -->
+		      	<!-- technician input -->
 		      	<div class="col-6 col-12-xsmall">
 		      <label for="technician"><b>Technician</b></label>
 		      <input type="text" placeholder="Enter technician name" name="technician" id="technician" ><br>
-		      <!-- <span class="error">*<?php echo $passwordErr;?></span></div> -->
     </div>
         <div class="col-6 col-12-xsmall">
           <label for="solution"><b>Solution</b></label>
           <input type="text" placeholder="Solution" name="solution" id="solution" ><br>
-          <!-- <span class="error">*<?php echo $passwordErr;?></span></div> -->
-    </div>    <div class="col-6 col-12-xsmall">
+    </div>    
+    <div class="col-6 col-12-xsmall">
           <label for="status"><b>Status</b></label>
           <select>
             <option value="New Arrived">New Arrived</option>
@@ -159,8 +156,7 @@ echo "<b>".date("d/M/Y")."</b>" ;?>" readonly=""><br>
             <option value="Completed">Completed</option>
             <option value="Dispatched">Dispatched</option>
           </select>
-          <!-- <input type="text" placeholder="" name="technician" id="technician" ><br> -->
-          <!-- <span class="error">*<?php echo $passwordErr;?></span></div> -->
+          <span class="error">*<?php echo $statusErr;?></span></div>
     </div>
   <input type="submit" name="submit" value="Submit">  
   <input type="reset" name="reset" value="Reset">
--- laptop.php
+++ laptop.php
@@ -1,79 +1,177 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>  
 <html>
 <head>
-	<title></title>
-	<style type="text/css">
-		input{
-			width: 100%;
-			font-size: 20px;
-			border-radius: 10px;
+			<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+<style>
+.error {color: #FF0000;}
+</style>
+<?php
+// define variables and set to empty values
+$customer_nameErr = $addressErr = $contactErr = $deviceErr = $statusErr = "";
+$customer_name = $address = $contact = $status = $solution = $technician = $device = $device_detail = $entry_number = $date = "";
 
-		}
-		td{
-			font-size: 20px;
-		}
-	</style>
-	
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $entry_number=test_input($_POST["entry_number"]);
+    $date=test_input($_POST["date"]);
+    $device_detail=test_input($_POST["device_detail"]);
+    $solution=test_input($_POST["solution"]);
+    $technician=test_input($_POST["technician"]);
+
+  if (empty($_POST["name"])) {
+   
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["customer_name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$customer_name)) {
+      $customer_nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["address"])) {
+    $addressErr = "Address is required";
+  } else{
+    $address = test_input($_POST["address"]);
+
+  }
+    
+  if (empty($_POST["contact"])) {
+    $contactErr = "Contact is required";
+  } else {
+    $contact = test_input($_POST["contact"]);
+
+  if (empty($_POST["device"])) {
+    $deviceErr = "device is required";
+  } else {
+    $device = test_input($_POST["device"]);
+  }
+
+  if (empty($_POST["status"])) {
+    $statusErr = "status is required";
+  } else {
+    $status = test_input($_POST["status"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
 </head>
-<body bgcolor="brown">
+<body>  
+<header id="header">
+    <a class="logo" href="index.php">A-CET Computers</a>
+    <nav>
+      <a href="#menu">Menu</a>
+    </nav>
+  </header>
+    <nav id="menu">
+      <ul class="links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Acet Shop</a></li>
+        <li><a href="#">Acet Courses</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="login.php" class="active">Log In</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="elements.php">Elements</a></li>
+      </ul>
+    </nav>
+      <div id="heading" >
+        <h1>Laptop entry</h1>
+      </div>
+<!-- <h2>PHP Form Validation Example</h2> -->
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-<!-- 	<p id="date"></p> -->
-<center>
-	<form style="background-color: white;border-color: black;border-radius: 20px;">
-		<h1>Laptop Entry</h1>
-		<table border="0" cellpadding="5">
-			<tr>
-				<td>	Entry number :</td>
-				<td><input type="text" id="entry_num"readonly=""></td>
-				<td></td>
-				<td> Entry Date : </td>
-				<td>
-					<p id="date"><input type="date" id="date" readonly=""></p></td>
-				
-			</tr>
-			<tr>
-				<td>Customer name : </td>
-				<td><input type="text" id="cName" required=""></td>
-				<td></td>
-				<td>Address : </td>
-				<td><input type="text" id="address"></td>
-			</tr>
-			<tr>
-				<td>Device name : </td>
-				<td><select><option>HP</option><option>DELL</option><option>TOSHIBA</option><option>FIJITSU</option><option>SAMSUNG</option><option>ACER</option><option>ASUS</option><option>E_MACHINE</option><option>I_BALL</option><option>MAC</option></select></td>
-				<td></td>
-				<td>Device Detail : </td>
-				<td><input type="text" id="address"></td>
-			</tr>
-			<tr>
+	<div class="row gtr-uniform">
+    <div class="col-6 col-12-xsmall">
+        <label for="entry number"><b>Entry Number</b></label>
+        <input type="text" placeholder="" name="entry_number" id="entry_number" value="" readonly=""><br>
+    </div>
+      <div class="col-6 col-12-xsmall">
+        <label for="Date"><b>Date :</b></label>
+        <input type="text" placeholder="" name="date" id="date" value="<?php
+              echo date("d/M/Y").date("D");?>" readonly=""><br>
+    </div>
+		    	<!-- customer name input -->
+		<div class="col-6 col-12-xsmall">
+	      <label for="customer_name"><b>Customer Name</b></label>
+	      <input type="text" placeholder="Enter Customer Name" name="customer_name" id="name" value=""><br>
+	      <span class="error"> *<?php echo $customer_nameErr;?></span>
+		</div>
+		<br><br>
+		      	<!-- contact input -->
+		<div class="col-6 col-12-xsmall">
+	      <label for="contact"><b>Contact</b></label>
+	      <input type="text" placeholder="Enter Contact" name="contact" id="contact" ><br>
+	      <span class="error">*<?php echo $contactErr;?></span>
+		</div>
+		<br><br>
+		<!-- address input-->
+		    	<div class="col-6 col-12-xsmall">
+		      <label for="address"><b>Address</b></label>
+		      <input type="text" placeholder="Enter address" name="address" id="address" value=""><br>
+		      <span class="error"> *<?php echo $addressErr;?></span></div>
+		<br><br>
+		      	<!-- device selection -->
+		      	<div class="col-6 col-12-xsmall">
+		      <label for="psw"><b>Device :</b></label>
+		     	<select name="device" id="category">
+						<option value="">- Select -</option>
+						<option value="Motherboard">HP</option>
+						<option value="Full CPU">Dell</option>
+						<option value="Monitor">Acer</option>
+						<option value="Others">Toshiba</option>
+						<option value="Others">Asus</option>
+						<option value="Others">Lenovo</option>
+						<option value="Others">Fujitsu</option>
+						<option value="Others">Compaq</option>
+						<option value="Others">Mac</option>
+						<option value="Others">emachine</option>
+						<option value="Others">Wipro</option>
+					</select>
+					<span class="error"> <?php echo $deviceErr;?></span>
+		<br><br>	
+		<!-- device detail -->
+		<div class="col-12 col-12-xsmall">
+		      <label for="device detail"><b>Device detail</b></label>
+		      <input type="text" placeholder="Enter device detail" name="device_detail" id="device_detail" value=""><br></div>
+		<br><br>
+		      	<!-- technician input -->
+		      	<div class="col-6 col-12-xsmall">
+		      <label for="technician"><b>Technician</b></label>
+		      <input type="text" placeholder="Enter technician name" name="technician" id="technician" ><br>
+    </div>
+        <div class="col-6 col-12-xsmall">
+          <label for="solution"><b>Solution</b></label>
+          <input type="text" placeholder="Solution" name="solution" id="solution" ><br>
+    </div>    
+    <div class="col-6 col-12-xsmall">
+          <label for="status"><b>Status</b></label>
+          <select>
+            <option value="New Arrived">New Arrived</option>
+            <option value="Waiting">Waiting</option>
+            <option value="Return">Return</option>
+            <option value="Completed">Completed</option>
+            <option value="Dispatched">Dispatched</option>
+          </select>
+          <span class="error">*<?php echo $statusErr;?></span></div>
+    </div>
+  <input type="submit" name="submit" value="Submit">  
+  <input type="reset" name="reset" value="Reset">
 
-				<td>Main Problem :</td>
-				<td><input type="text" id="problem" width="100%"></td>
-				<td></td>
-				<td>Contact : </td>
-				<td><input type="text" id="phone"></td>
-			</tr>
-			<tr>
-				<td>technician Id				</td>
-				<td><input type="text" id="tech_id"></td>
-				<td></td>
-				<td>Status</td><td><input type="text" id="status"></td>
-			</tr>
+</form>
 
-			<tr>
-				<td></td><td></td><td><input type="submit" value="Enter"></td><td><input type="button" value="Show all records"></td><td><a href="office system.html"><input type="button" value="Back">
-					
-				</a></td>
-			</tr>
-		</table>
-	</form>
-</center>
-<script type="text/javascript">
-		d= new Date();
-		h=d.getHours
-		m=d.getMinutes
-		date=h :m
-		document.getElementById("date").innerHTML= date
-	</script>
+
+
 </body>
 </html>
+ 
