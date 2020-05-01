78f4cf350317f4eb164b8b559033f678000587ca
krishna-bhandari <59533492+krishna-bhandari@users.noreply.github.com>
2020-05-01 21:51:14 +0545

    change

--- khalti-checkout-example-master/example.js
+++ khalti-checkout-example-master/example.js
@@ -1,16 +1,16 @@
 var config = {
     // replace the publicKey with yours
-    "publicKey": "live_public_key_969c3f06eca040728f13a2351d765707",
+    "publicKey": "test_public_key_02a945a700104a5c87e65e8a8769971b",
     "productIdentity": "1234567890",
     "productName": "Dragon",
     "productUrl": "http://gameofthrones.wikia.com/wiki/Dragons",
     "eventHandler": {
         onSuccess(payload) {
             // hit merchant api for initiating verfication
-            // console.log(payload);
+            console.log(payload);
         },
         onError(error) {
-            // console.log(error);
+            console.log(error);
         }
     }
 };
